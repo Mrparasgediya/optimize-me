e77388240dbf831997019374c0e7168f5936be62
Paras Gediya <mrparas103@gmail.com>
2021-07-09 00:05:31 -0700

    make some changes to app

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 const { mongodb_connection_url } = require("../config");
-mongoose.connect(mongodb_connection_url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+mongoose.connect(
+  mongodb_connection_url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (error) => {
+    if (error) console.log("mongoose connection error ", error);
+    else console.log("successfull connection to db");
+  }
+);
